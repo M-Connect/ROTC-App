0f62dc9dcf0b28a80f4ff884a66568c4b2bbaa80
cthomasn <60117921+cthomasn@users.noreply.github.com>
2021-02-08 13:32:26 -0500

    Required fields indication message and rerouting.
    Rerouted to signInPage instead of welcomePage.

--- rotc_app/lib/app/registration_page/registrationPage.dart
+++ rotc_app/lib/app/registration_page/registrationPage.dart
@@ -2,13 +2,14 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// Author: Christine Thomas
-// This class creates the UI for the RegistrationPage
-// TextFormFields to be modularized
-// validation needs to be fixed
+/*
+ Author: Christine Thomas
+ This class creates the UI for the RegistrationPage.
+ TODO: TextFormFields to be modularized and validation needs to be fixed.
 
-//CoAuthor:  Kyle Serruys
-//This class now has the functionality to connect to our cadets database
+  Co-Author:  Kyle Serruys
+  This class now has the functionality to connect to our cadets database.
+*/
 
 // ignore: must_be_immutable
 class RegistrationPage extends StatelessWidget {
@@ -43,47 +44,33 @@ class RegistrationPage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.fromLTRB(25.0, 30.0, 35.0, 8.0),
+          padding: EdgeInsets.fromLTRB(25.0, 19.0, 35.0, 8.0),
           width: MediaQuery.of(context).size.width,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
-                child: Text(
-                  'First Name *',
-                ),
-              ),
-              TextFormField(
-                controller: fName,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'John',
-                ),
-                onSaved: (String value) {},
-                validator: (String value) {
-                  return value.contains('_')
-                      ? 'Letters and hyphens only.'
-                      : null;
-                },
-              ),
-              spaceBetweenFields,
-              Column(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Padding(
+                    padding: const EdgeInsets.only(left: 3.0, bottom: 18.0),
+                    child: Text(
+                      '* indicates a required field.',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                  ),
+                  Padding(
                     padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
                     child: Text(
-                      'Last Name *',
+                      'First Name *',
                     ),
                   ),
                   TextFormField(
-                    controller: lName,
+                    controller: fName,
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
-                      hintText: 'Doe',
+                      hintText: 'John',
                     ),
                     onSaved: (String value) {},
                     validator: (String value) {
@@ -92,127 +79,152 @@ class RegistrationPage extends StatelessWidget {
                           : null;
                     },
                   ),
-                ],
-              ),
-              spaceBetweenFields,
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
-                    child: Text(
-                      'Nickname ',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: nName,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: '\"The Deer\"',
-                    ),
-                    onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.contains('_')
-                          ? 'Only type in letters and numbers.'
-                          : null;
-                    },
-                  ),
-                ],
-              ),
-              spaceBetweenFields,
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
-                    child: Text(
-                      'Email  *',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: email,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: 'johndoe@rotc.com',
-                    ),
-                    onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.contains('@')
-                          ? null
-                          : 'Invalid email type, must contain @ symbol.';
-                    },
-                  ),
-                ],
-              ),
-              spaceBetweenFields,
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
-                    child: Text(
-                      'Password *',
-                    ),
-                  ),
-                  TextFormField(
-                    controller: password,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                    onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.isEmpty ? 'Field cannot be blank.' : null;
-                    },
-                  ),
-                ],
-              ),
-              spaceBetweenFields,
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
-                    child: Text(
-                      'Confirm Password *',
-                    ),
-                  ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                    ),
-                    obscureText: true,
-                    onSaved: (String value) {},
-                    validator: (String value) {
-                      return value.isEmpty ? 'Field cannot be blank.' : null;
-                    },
-                  ),
-                  Container(
-                    child: Padding(
-                      padding:
-                          const EdgeInsets.fromLTRB(247.0, 12.0, 0.0, 30.0),
-                      child: ElevatedButton(
-                        child: Text('Register'),
-                        onPressed: () async {
-                          await FirebaseAuth.instance
-                              .createUserWithEmailAndPassword(
-                                  email: email.text, password: password.text);
-                          await userRegistration();
-                          Navigator.pushNamed(context, '/');
+                  spaceBetweenFields,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
+                        child: Text(
+                          'Last Name *',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: lName,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Doe',
+                        ),
+                        onSaved: (String value) {},
+                        validator: (String value) {
+                          return value.contains('_')
+                              ? 'Letters and hyphens only.'
+                              : null;
                         },
                       ),
-                    ),
+                    ],
+                  ),
+                  spaceBetweenFields,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
+                        child: Text(
+                          'Nickname ',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: nName,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: '\"The Deer\"',
+                        ),
+                        onSaved: (String value) {},
+                        validator: (String value) {
+                          return value.contains('_')
+                              ? 'Only type in letters and numbers.'
+                              : null;
+                        },
+                      ),
+                    ],
+                  ),
+                  spaceBetweenFields,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
+                        child: Text(
+                          'Email  *',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: email,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'johndoe@rotc.com',
+                        ),
+                        onSaved: (String value) {},
+                        validator: (String value) {
+                          return value.contains('@')
+                              ? null
+                              : 'Invalid email type, must contain @ symbol.';
+                        },
+                      ),
+                    ],
+                  ),
+                  spaceBetweenFields,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
+                        child: Text(
+                          'Password *',
+                        ),
+                      ),
+                      TextFormField(
+                        controller: password,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                        onSaved: (String value) {},
+                        validator: (String value) {
+                          return value.isEmpty ? 'Field cannot be blank.' : null;
+                        },
+                      ),
+                    ],
+                  ),
+                  spaceBetweenFields,
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 3.0, bottom: 6.0),
+                        child: Text(
+                          'Confirm Password *',
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                        ),
+                        obscureText: true,
+                        onSaved: (String value) {},
+                        validator: (String value) {
+                          return value.isEmpty ? 'Field cannot be blank.' : null;
+                        },
+                      ),
+                      Container(
+                        child: Padding(
+                          padding:
+                              const EdgeInsets.fromLTRB(247.0, 12.0, 0.0, 30.0),
+                          child: ElevatedButton(
+                            child: Text('Register'),
+                            onPressed: () async {
+                              await FirebaseAuth.instance
+                                  .createUserWithEmailAndPassword(
+                                      email: email.text, password: password.text);
+                              await userRegistration();
+                              ///Sending to signInPage instead of welcomePage -Christine
+                              Navigator.pushNamed(context, '/signIn');
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ],
           ),
         ),
-      ),
     );
   }
 }
