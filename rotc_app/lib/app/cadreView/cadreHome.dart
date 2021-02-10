3ee22df019de5036290cb4489c038243987fb605
cthomasn <60117921+cthomasn@users.noreply.github.com>
2021-02-10 16:35:05 -0500

    Add files via upload

--- rotc_app/lib/app/cadreView/cadreHome.dart
+++ rotc_app/lib/app/cadreView/cadreHome.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 /*
   Author: Christine Thomas
@@ -8,6 +9,7 @@ import 'package:flutter/material.dart';
 
 class CadreHome extends StatefulWidget {
   CadreHome({Key key}) : super(key: key);
+
   @override
   _CadreHomeState createState() => _CadreHomeState();
 }
@@ -16,12 +18,15 @@ class CadreHome extends StatefulWidget {
 class _CadreHomeState extends State<CadreHome> {
   int _tabOption = 0;
 
-  static List<Widget> _widgetOptions = <Widget>[
-    dashboard(),
-    peerReviewForm(),
-    messages(),
-    profile()
-  ];
+  List<Widget> tabThrough() {
+    List<Widget> _widgetOptions = <Widget>[
+      dashboard(),
+      peerReviewForm(),
+      messages(),
+      profile()
+    ];
+    return _widgetOptions;
+  }
 
   void _chosenTab(int index) {
     setState(() {
@@ -37,7 +42,7 @@ class _CadreHomeState extends State<CadreHome> {
         centerTitle: true,
       ),
       body: Center(
-        child: _widgetOptions.elementAt(_tabOption),
+        child: tabThrough().elementAt(_tabOption),
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
@@ -59,9 +64,137 @@ class _CadreHomeState extends State<CadreHome> {
           ),
         ],
         currentIndex: _tabOption,
-        unselectedItemColor: Theme.of(context).primaryColorLight,
-        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Theme
+            .of(context)
+            .primaryColorLight,
+        selectedItemColor: Theme
+            .of(context)
+            .accentColor,
         onTap: _chosenTab,
+      ),
+    );
+  }
+
+  /// NOT FUNCTIONAL YET
+  Widget profile() {
+    const TextStyle tabTextStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    const TextStyle ranking =
+    TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
+    final _textController = TextEditingController();
+    String biography = '';
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 25.0, bottom: 15.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: CircleAvatar(
+                    backgroundColor: Colors.lightBlueAccent[200],
+                    child: Text(
+                      'JD',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.amber[200],
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    radius: 75.0,
+                  ),
+                ),
+              ),
+              Text(
+                'Cadre Doe',
+                style: tabTextStyle,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Technical Sergeant',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+              Divider(
+                thickness: 1,
+              ),
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(0.0, 10.0, 255.0, 10.0),
+                    child: GestureDetector(
+                      child: Text(
+                        'Biography',
+                        style: TextStyle(
+                          fontSize: 15,
+                        ),
+                      ),
+                      onTap: () {
+                        AlertDialog(
+                          title: Text(
+                            'Biography',
+                          ),
+                          content: TextField(
+                            onChanged: (value) {
+                              setState(() {
+                                biography = value;
+                              });
+                            },
+                            controller: _textController,
+                          ),
+                          actions: [
+                            FlatButton(
+                              color: Colors.amber[200],
+                              textColor: Colors.white,
+                              child: Text('Done'),
+                              onPressed: () {
+                                setState(() {
+
+                                });
+                              },
+                            )
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    width: 330.0,
+                    child: TextField(
+                      controller: _textController,
+                      decoration: InputDecoration(
+                        hintText: 'Tell us about yourself',
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.elliptical(10, 10),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10.0),
+                          ),
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+//maxLines: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -69,24 +202,24 @@ class _CadreHomeState extends State<CadreHome> {
 
 Widget dashboard() {
   const TextStyle tabTextStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
   return Scaffold(
     body: SingleChildScrollView(
       child: Container(
           child: Row(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'On Your Radar',
-              style: tabTextStyle,
-            ),
-          ),
-          Column(
-            children: [],
-          ),
-        ],
-      )),
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'On Your Radar',
+                  style: tabTextStyle,
+                ),
+              ),
+              Column(
+                children: [],
+              ),
+            ],
+          )),
     ),
   );
 }
@@ -139,26 +272,3 @@ Widget messages() {
   );
 }
 
-Widget profile() {
-  const TextStyle tabTextStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  return Scaffold(
-    body: SingleChildScrollView(
-      child: Container(
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Profile',
-                  style: tabTextStyle,
-                ),
-              ),
-              Column(
-                children: [],
-              ),
-            ],
-          )),
-    ),
-  );
-}
