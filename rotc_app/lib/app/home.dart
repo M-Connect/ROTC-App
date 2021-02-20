867b09cd28844340c4e36eee2130d3f8a31c49e7
cthomasn <60117921+cthomasn@users.noreply.github.com>
2021-02-20 10:54:38 -0500

    Changed Biography and added Edit Icon Button
    Tried multiple ways to get it to work, not successful yet

--- rotc_app/lib/app/home.dart
+++ rotc_app/lib/app/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 import '../main.dart';
 
@@ -199,27 +200,106 @@ Widget messages() {
   );
 }
 
-Widget profile() {
+Widget profile(){
   const TextStyle tabTextStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  const TextStyle ranking =
+  TextStyle(fontSize: 18, fontWeight: FontWeight.normal);
+  final _textController = TextEditingController();
+  final editDialog =  SimpleDialog(
+    title: Text(
+        'Edit Biography'
+    ),
+    children: [
+      TextField(
+        onChanged: (text) {
+          print("First text field: $text");
+        },
+      ),
+
+    ],
+  );
+  final icon =  Row(
+  mainAxisSize: MainAxisSize.min,
+  children: [
+  IconButton(
+      icon: Icon(
+        Icons.edit_outlined,
+          color: Colors.amber[200],
+      ),
+    onPressed: (){
+    },
+  ),
+  ],
+  );
+
   return Scaffold(
-    body: SingleChildScrollView(
-      child: Container(
-        child: Row(
+    body: Container(
+        child: Column(
           children: [
             Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                'Profile',
-                style: tabTextStyle,
+              padding: const EdgeInsets.only(top: 25.0, bottom: 15.0),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: CircleAvatar(
+                  backgroundColor: Colors.lightBlueAccent[200],
+                  child: Text(
+                    'JD',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.amber[200],
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  radius: 75.0,
+                ),
               ),
             ),
-            Column(
-              children: [],
+            Text(
+              'Cadre Doe',
+              style: tabTextStyle,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Technical Sergeant',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Divider(
+              thickness: 1,
+            ),
+
+      Container(
+    padding: EdgeInsets.only(left: 20, right: 20),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Biography',
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.w400,
+            fontSize: 20,
+          ),
+        ),
+        icon,
+      ],
+    ),
+  ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(40.0, 0.0, 255.0, 0.0),
+    ),
+
+              ],
             ),
           ],
         ),
       ),
-    ),
   );
 }
