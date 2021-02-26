e658751876acf836b1bbcca7983ecdea8e13e357
Mac-Rufus Umeokolo <onyimacr@gmail.com>
2021-02-26 18:33:39 -0500

    changed the widget dashboard into a class dashboard - MRU

--- rotc_app/lib/app/dashboard/dashboard.dart
+++ rotc_app/lib/app/dashboard/dashboard.dart
@@ -1,44 +1,51 @@
 
 import 'package:flutter/material.dart';
 import 'package:rotc_app/main.dart';
+import 'package:flutter/material.dart';
+import '../../main.dart';
 
-Widget dashboard() {
-  const TextStyle tabTextStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  return Scaffold(
-    body: Container(
-      padding: EdgeInsets.all(25.0),
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Container(
-            child: ElevatedButton(
-              child: Text('Review Request Anouncement'),
-              onPressed: () {
-                navigation.currentState.pushNamed('');
-              },
+class dashboard extends StatelessWidget {
+
+  bool isCadre = false;
+  @override
+  Widget build(BuildContext context) {
+    const TextStyle tabTextStyle =
+    TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(25.0),
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: ElevatedButton(
+                child: Text('Review Request Anouncement'),
+                onPressed: () {
+                  navigation.currentState.pushNamed('');
+                },
+              ),
             ),
-          ),
-          Container(
-            child: ElevatedButton(
-              child: Text('Lead Lab OPORD'),
-              onPressed: () {
-                navigation.currentState.pushNamed('');
-              },
+            Container(
+              child: ElevatedButton(
+                child: Text('Lead Lab OPORD'),
+                onPressed: () {
+                  navigation.currentState.pushNamed('');
+                },
+              ),
             ),
-          ),
-          Container(
-            child: ElevatedButton(
-              child: Text('PT ConOps'),
-              onPressed: () {
-                navigation.currentState.pushNamed('');
-              },
+            Container(
+              child: ElevatedButton(
+                child: Text('PT ConOps'),
+                onPressed: () {
+                  navigation.currentState.pushNamed('');
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
