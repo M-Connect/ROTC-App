df60a5459d7d7d28a1f753df3a206cdc83f440b4
cthomasn <60117921+cthomasn@users.noreply.github.com>
2021-02-27 09:45:52 -0500

    Fixed tabOption routing to the old Profile page.

--- rotc_app/lib/app/messaging/messaging.dart
+++ rotc_app/lib/app/messaging/messaging.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rotc_app/app/profile/profile.dart';
 
 /*
   Author: Christine Thomas
@@ -22,7 +23,7 @@ class _CadreHomeState extends State<HomeView> {
     dashboard(),
     peerReviewForm(),
     messages(),
-    profile()
+    Profile(),
   ];
 
   void _chosenTab(int index) {
@@ -130,30 +131,6 @@ Widget messages() {
                 padding: const EdgeInsets.all(20.0),
                 child: Text(
                   'Messages',
-                  style: tabTextStyle,
-                ),
-              ),
-              Column(
-                children: [],
-              ),
-            ],
-          )),
-    ),
-  );
-}
-
-Widget profile() {
-  const TextStyle tabTextStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  return Scaffold(
-    body: SingleChildScrollView(
-      child: Container(
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  'Profile',
                   style: tabTextStyle,
                 ),
               ),
