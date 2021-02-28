11bec7785a7a0bb48460c9d458fc06e7ab76ace9
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2021-02-28 11:58:41 -0500

    Added functionality

--- rotc_app/lib/app/peerReview/peerReviewRequest.dart
+++ rotc_app/lib/app/peerReview/peerReviewRequest.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-
+import 'package:rotc_app/app/peerReview/peerReviewLanding.dart';
 
 /*
- Author: Kyle Serruys
+ Author: Sawyer Kisha
+ Co-author: Kyle Serruys
   This class is the home page for our peer review request page
+  Needs functionality
  */
 class PeerReviewRequest extends StatelessWidget {
   @override
@@ -14,12 +16,152 @@ class PeerReviewRequest extends StatelessWidget {
         actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.logout),
-            onPressed: () {
+            onPressed: (){},
 
-            },
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(25.0),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              //Selecting a Cadet
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom:10.0),
+                child: Text('Cadet Name:'),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget> [
+                    Container(
+                      width: 200.0, //Box length
+                      child: new DropdownButton<String>(
+                        underline: Container(
+                          height: 2,
+                          decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          ),
+                        ),
+                      hint: Text('Select a Cadet'),
+                  //Temporary placements (will fix with actual users in future)
+                  items: <String>['John', 'James', 'Jones', 'Jackson'].map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: new Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+                    ),
+                ],
+                ),
+              ),
+              SizedBox(
+                width: 900,
+               child: Row(
+                 children: <Widget>[
+                 Radio(value: 100, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                 Text('AS100'),
+                 Radio(value: 200, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                 Text('AS200'),
+                 Radio(value: 300, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                 Text('AS300'),
+                 Radio(value: 400, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                 Text('AS400'),
+                 ],
+               ),
+              ),
+              SizedBox(
+                width: 900,
+                child: Row(
+                  children: <Widget>[
+                    Radio(value: 500, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                    Text('AS500'),
+                    Radio(value: 600, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                    Text('AS600'),
+                    Radio(value: 700, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                    Text('AS700'),
+                    Radio(value: 800, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                    Text('AS800'),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: 900,
+                child: Row(
+                  children: <Widget>[
+                    Radio(value: 0, activeColor: Colors.black87, groupValue: null, onChanged: null), //test value
+                    Text('GMC'),
+                    Radio(value: 1, activeColor: Colors.black87, groupValue: null, onChanged: null), //test value
+                    Text('POC'),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom:10.0),
+                child: Text('Specify Reviewer(s): '),
+              ),
+              Container(
+                width: 200.0, //Box length
+                child: new DropdownButton<String>(
+                  underline: Container(
+                    height: 2,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
+                  ),
+                  hint: Text('Select reviewer(s)'),
+                  //Temporary placements (will fix with actual users in future)
+                  items: <String>['John', 'James', 'Jones', 'Jackson'].map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: new Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
+                ),
+              ),
+
+              //Peer Review for:
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom:10.0),
+                child: Text('Evaluation for:'),
+              ),
+              Container(
+                width: 400.0,
+                child: TextFormField(
+                  textAlignVertical: TextAlignVertical.top,
+                  maxLength: 160,
+                  maxLengthEnforced: true,
+                  maxLines: 10,
+                  //controller:
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    contentPadding: EdgeInsets.all(10.0),
+                    // const EdgeInsets.symmetric(vertical: 75.0),
+                  ),
+                  onSaved: (String value) {},
+                ),
+                ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+          padding:
+          EdgeInsets.only(bottom: 20.0, left: 10.0, top: 20.0, right: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Submit'),
+                onPressed: () async {},
+              ),
+            ],
+          )),
     );
   }
 }
