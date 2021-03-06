f448d57e8000c2665e0b34b6cd24fe614d848c5c
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2021-03-06 12:33:16 -0500

    Added Cadet selection to peer review and request

--- rotc_app/lib/app/peerReview/peerReview.dart
+++ rotc_app/lib/app/peerReview/peerReview.dart
@@ -1,58 +1,136 @@
 import 'package:flutter/material.dart';
-
 import 'package:rotc_app/app/peerReview/peerReviewLanding.dart';
-
+import 'package:rotc_app/common_widgets/buttonWidgets.dart';
 import '../../main.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+/*
+ Author: Sawyer Kisha
+ Co-author: Kyle Serruys
+  This class is the home page for our peer review request page
+  Needs functionality
+ */
+
+class PeerReview extends StatefulWidget {
+  @override
+  PeerReviewState createState() => PeerReviewState();
+
+}
+
+//Setting how many togglebuttons may be selected
+List<bool> isSelected = List.generate(4, (index) => false);
+
+class PeerReviewState extends State<PeerReview> {
+  String firstName = "";
+  String lastName = "";
+
+  CollectionReference cadets = FirebaseFirestore.instance.collection('cadets');
 
 
+  Future<void> peerReview(){
+    return cadets.add({
+      "firstName": firstName,
+      "lastName": lastName,
+    });
+  }
 
-class PeerReview extends StatelessWidget {
+  @override
+  void initState(){
+    getCadetNames();
+  }
+
+  getCadetNames() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState((){
+      firstName = prefs.getString("firstName");
+      lastName = prefs.getString("lastName");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Peer Review'),
+        title: Text('Evaluation Selection'),
         actions: <Widget>[
           new IconButton(
-            icon: new Icon(Icons.logout),
-            onPressed: signOut,
+              icon: new Icon(Icons.logout),
+              onPressed: () {
+                alertSignOut(context);
+              }
           ),
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(25.0),
-        color: Colors.white,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-              child: ElevatedButton(
-                child: Text('LLAB 2 FT Presentation Peer Review'),
-                onPressed: () {
+
+      body: SingleChildScrollView(
+          padding: EdgeInsets.all(25.0),
+          child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:[
+                    Padding(
+
+                      padding: const EdgeInsets.only(top: 50.0, bottom: 70.0),
+                      child: Text('Select a Cadet:'),
+
+                    ),
+                    Container(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              ToggleButtons(
+                                children:<Widget>[
+                                  Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("c t",)],)),
+                                  Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("Sawyer K",)],)),
+                                  Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("mac creed",)],)),
+                                  Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("Test man",)],)),
+                                ],
+                                isSelected: isSelected,
+                                  onPressed: (int index) {
+                                    setState(() {
+                                      for (int buttonIndex = 0; buttonIndex < isSelected.length; buttonIndex++) {
+                                        if (buttonIndex == index) {
+                                          isSelected[buttonIndex] = true;
+                                        } else {
+                                          isSelected[buttonIndex] = false;
+                                        }
+                                      }
+                                    });
+                                  },
+                                //isSelected: isSelected,
+                              ),
+                            ]
+                        )
+                      // child: Text(firstName ??),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                    ),
+
+                  ]
+              )
+          )
+      ),
+
+
+
+      bottomNavigationBar: Padding(
+          padding:
+          EdgeInsets.only(bottom: 40.0, left: 10.0, top: 40.0, right: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Next'),
+                onPressed: () async {
                   navigation.currentState.pushNamed('/peerReviewLLAB2FT');
                 },
               ),
-            ),
-            Container(
-              child: ElevatedButton(
-                child: Text('FLX flight Commander Peer Review'),
-                onPressed: () {
-                  navigation.currentState.pushNamed('/peerReviewFLXFlight');
-                },
-              ),
-            ),
-            Container(
-              child: ElevatedButton(
-                child: Text('Commissioning Planning Peer Review'),
-                onPressed: () {
-                  navigation.currentState.pushNamed('/peerReviewCommissioning');
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          )),
     );
   }
 }
--- rotc_app/lib/app/peerReview/peerReviewRequest.dart
+++ rotc_app/lib/app/peerReview/peerReviewRequest.dart
@@ -1,5 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:rotc_app/app/peerReview/peerReviewLanding.dart';
+import 'package:rotc_app/common_widgets/buttonWidgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../main.dart';
 
 /*
  Author: Sawyer Kisha
@@ -7,20 +11,129 @@ import 'package:rotc_app/app/peerReview/peerReviewLanding.dart';
   This class is the home page for our peer review request page
   Needs functionality
  */
-class PeerReviewRequest extends StatelessWidget {
+
+class PeerReviewRequest extends StatefulWidget {
+  @override
+  PeerReviewRequestState createState() => PeerReviewRequestState();
+
+}
+
+//Setting how many togglebuttons may be selected
+List<bool> isSelected = List.generate(4, (index) => false);
+
+class PeerReviewRequestState extends State<PeerReviewRequest> {
+  String firstName = "";
+  String lastName = "";
+
+  CollectionReference cadets = FirebaseFirestore.instance.collection('cadets');
+
+
+  Future<void> peerReview(){
+    return cadets.add({
+      "firstName": firstName,
+      "lastName": lastName,
+    });
+  }
+
+  @override
+  void initState(){
+    getCadetNames();
+  }
+
+  getCadetNames() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState((){
+      firstName = prefs.getString("firstName");
+      lastName = prefs.getString("lastName");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Peer Review request'),
+        title: Text('Peer Review Request'),
         actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.logout),
-            onPressed: (){},
-
-          ),
+        onPressed: () {
+          alertSignOut(context);
+        }
+        ),
         ],
       ),
+
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(25.0),
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+              Padding(
+
+                padding: const EdgeInsets.only(top: 50.0, bottom: 70.0),
+                child: Text('Select Cadet(s):'),
+
+              ),
+              Container(
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                        ToggleButtons(
+                          children:<Widget>[
+                            Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("c t",)],)),
+                            Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("Sawyer K",)],)),
+                            Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("mac creed",)],)),
+                            Container(width: (MediaQuery.of(context).size.width - 72)/4, child: new Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[new SizedBox(width: 4.0,), new Text("Test man",)],)),
+                          ],
+                          isSelected: isSelected,
+                          onPressed: (int index){
+                            setState((){
+                              isSelected[index] = !isSelected[index];
+                            });
+                          },
+                          borderWidth: 3,
+                          borderColor: Colors.lightBlue,
+                          hoverColor: Colors.grey,
+                          //isSelected: isSelected,
+                        ),
+                  ]
+                )
+                // child: Text(firstName ??),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              ),
+
+            ]
+          )
+        )
+      ),
+
+
+
+      bottomNavigationBar: Padding(
+          padding:
+          EdgeInsets.only(bottom: 40.0, left: 10.0, top: 40.0, right: 10.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+
+            children: <Widget>[
+              ElevatedButton(
+                child: Text('Submit'),
+                onPressed: () async {
+                  navigation.currentState.pushNamed('/homePage');
+                },
+              ),
+            ],
+          )),
+      );
+  }
+}
+
+
+/*
       body: SingleChildScrollView(
         padding: EdgeInsets.all(25.0),
         child: Form(
@@ -34,72 +147,63 @@ class PeerReviewRequest extends StatelessWidget {
                 child: Text('Cadet Name:'),
               ),
               Container(
-                child: Column(
-                  children: <Widget> [
-                    Container(
-                      width: 200.0, //Box length
-                      child: new DropdownButton<String>(
-                        underline: Container(
-                          height: 2,
-                          decoration: BoxDecoration(
-                          border: Border.all(color: Colors.black),
-                          ),
-                        ),
-                      hint: Text('Select a Cadet'),
-                  //Temporary placements (will fix with actual users in future)
-                  items: <String>['John', 'James', 'Jones', 'Jackson'].map((String value) {
-                    return new DropdownMenuItem<String>(
-                      value: value,
-                      child: new Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (_) {},
-                ),
+                width: 200.0, //Box length
+                child: new DropdownButton<String>(
+                  underline: Container(
+                    height: 2,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black),
                     ),
+                  ),
+                  hint: Text('Select a Cadet'),
+                  //Temporary placements (will fix with actual users in future)
+                items: <String>['John', 'James', 'Jones', 'Jackson'].map((String value) {
+                  return new DropdownMenuItem<String>(
+                    value: value,
+                    child: new Text(value),
+                  );
+                }).toList(),
+                onChanged: (_) {},
+              ),
+              ),
+              //Radio buttons 100 - 400 (Two rows)
+              ButtonBar(
+                alignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Radio(value: 100, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('100'),
+                  Radio(value: 200, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('200'),
+                  Radio(value: 300, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('300'),
+                  Radio(value: 400, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('400'),
                 ],
-                ),
               ),
-              SizedBox(
-                width: 900,
-               child: Row(
-                 children: <Widget>[
-                 Radio(value: 100, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                 Text('AS100'),
-                 Radio(value: 200, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                 Text('AS200'),
-                 Radio(value: 300, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                 Text('AS300'),
-                 Radio(value: 400, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                 Text('AS400'),
+
+              //Radio buttons 500 - 800 (Two rows)
+              ButtonBar(
+                  alignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                  Radio(value: 700, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('700'),
+                  Radio(value: 800, activeColor: Colors.black87, groupValue: null, onChanged: null),
+                  Text('800'),
                  ],
-               ),
               ),
-              SizedBox(
-                width: 900,
-                child: Row(
+
+              //Radio buttons GMC and POC
+              ButtonBar(
+                  alignment: MainAxisAlignment.start,
                   children: <Widget>[
-                    Radio(value: 500, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                    Text('AS500'),
-                    Radio(value: 600, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                    Text('AS600'),
-                    Radio(value: 700, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                    Text('AS700'),
-                    Radio(value: 800, activeColor: Colors.black87, groupValue: null, onChanged: null),
-                    Text('AS800'),
-                  ],
-                ),
-              ),
-              SizedBox(
-                width: 900,
-                child: Row(
-                  children: <Widget>[
-                    Radio(value: 0, activeColor: Colors.black87, groupValue: null, onChanged: null), //test value
+                    Radio(value: 0, activeColor: Colors.black87, groupValue: null, onChanged: null), //Temp value set to 0
                     Text('GMC'),
-                    Radio(value: 1, activeColor: Colors.black87, groupValue: null, onChanged: null), //test value
+                    Radio(value: 1, activeColor: Colors.black87, groupValue: null, onChanged: null), //Temp calue set to 1
                     Text('POC'),
                   ],
-                ),
               ),
+
+              //Specify Reviewers
               Padding(
                 padding: const EdgeInsets.only(top: 10.0, bottom:10.0),
                 child: Text('Specify Reviewer(s): '),
@@ -131,37 +235,27 @@ class PeerReviewRequest extends StatelessWidget {
                 child: Text('Evaluation for:'),
               ),
               Container(
-                width: 400.0,
-                child: TextFormField(
-                  textAlignVertical: TextAlignVertical.top,
-                  maxLength: 160,
-                  maxLengthEnforced: true,
-                  maxLines: 10,
-                  //controller:
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    contentPadding: EdgeInsets.all(10.0),
-                    // const EdgeInsets.symmetric(vertical: 75.0),
+                width: 200.0, //Box length
+                child: new DropdownButton<String>(
+                  underline: Container(
+                    height: 2,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black),
+                    ),
                   ),
-                  onSaved: (String value) {},
+                  hint: Text('Select an event'),
+                  //Temporary placements (will fix with actual users in future)
+                  items: <String>['Activity', 'Exercise', 'Run', 'Weights'].map((String value) {
+                    return new DropdownMenuItem<String>(
+                      value: value,
+                      child: new Text(value),
+                    );
+                  }).toList(),
+                  onChanged: (_) {},
                 ),
-                ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: Padding(
-          padding:
-          EdgeInsets.only(bottom: 20.0, left: 10.0, top: 20.0, right: 10.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                child: Text('Submit'),
-                onPressed: () async {},
               ),
             ],
-          )),
-    );
-  }
-}
+        ),
+      ),
+      ),
+      */
