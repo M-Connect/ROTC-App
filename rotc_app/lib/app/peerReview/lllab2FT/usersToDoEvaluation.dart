63ce6ac58de59f88984807238884a1f3ea096699
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2021-03-10 13:08:23 -0500

    Formatting and fixes

---
+++ rotc_app/lib/app/peerReview/lllab2FT/usersToDoEvaluation.dart
@@ -0,0 +1,164 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:rotc_app/common_widgets/buttonWidgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../../../main.dart';
+
+class UsersToDoEvaluation extends StatefulWidget {
+  @override
+  _UsersToDoEvaluationState createState() => _UsersToDoEvaluationState();
+}
+
+class _UsersToDoEvaluationState extends State<UsersToDoEvaluation> {
+  var userList = new List<String>();
+  var usersToEvaluate = new List<String>();
+  var usersToDoEvaluation = new List<String>();
+  var selectUsersList = new List<String>();
+
+  List<ElevatedButton> userButtonList = new List<ElevatedButton>();
+  String firstName = "";
+  String lastName = "";
+
+  CollectionReference evaluationRequests = FirebaseFirestore.instance.collection('userEvaluationRequests');
+
+  Future<void> userEvaluationRequests() {
+    selectUsersList.forEach((evaluator) {
+      usersToEvaluate.forEach((evaluatee) {
+        return evaluationRequests.add({
+          "evaluator": evaluator,
+          "evaluatee":evaluatee,
+          "status":"Pending"
+        });
+      });
+    });
+
+
+    /*return evaluationRequests.add({
+      "usersToEvaluate": usersToEvaluate,
+      "usersToDoEvaluation": selectUsersList,
+    });*/
+  }
+
+/*
+Author:  Kyle Serruys
+
+  */
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+    getUsersToEvaluate();
+  }
+
+  getUsersToEvaluate() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    usersToEvaluate = prefs.getStringList("usersToEvaluate");
+  }
+
+  getUserInfo() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var data = await FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then((docSnapshot) {
+      docSnapshot.docs.forEach((element) {
+        userList.add(element.data()['firstName'].toString() +
+            element.data()['lastName'].toString());
+      });
+    });
+    setState(() {});
+  }
+
+  List<Widget> makeButtonsList() {
+    for (int i = 0; i < userList.length; i++) {
+      userButtonList.add(
+        new ElevatedButton(
+          onPressed: () async {
+            SharedPreferences prefs = await SharedPreferences.getInstance();
+            selectUsersList.add(userList[i]);
+          },
+          child: Container(
+              width: 200,
+              height: 40,
+              child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:<Widget>[
+                    Text(userList[i])
+                  ]
+              )
+          ),
+        ),
+      );
+    }
+    return userButtonList;
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Peer Review Request'),
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(Icons.logout),
+              onPressed: () {
+                alertSignOut(context);
+              }),
+        ],
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(25.0),
+        child: Container(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 50.0, bottom: 50.0),
+                    child: Container(
+                      child: Text('Select Cadet(s) To Evaluate The Previously Selected Cadet(s)              (Self Evaluation is Accepted):',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                       ),
+                     )
+                  ),
+                Container(
+                  child: Column(
+                    children: makeButtonsList(),
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: <Widget>[
+
+                    ],
+                  ),
+                ),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+                ),
+              ]),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding:
+        EdgeInsets.only(bottom: 40.0, left: 10.0, top: 40.0, right: 10.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              child: Text('Submit'),
+              onPressed: () async {
+                SharedPreferences prefs= await SharedPreferences.getInstance();
+                await userEvaluationRequests();
+                prefs.remove("usersToEvaluate");
+                navigation.currentState.pushNamed('/homePage');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
