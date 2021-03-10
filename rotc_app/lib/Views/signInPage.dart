d3e656e89a67a5c26b6f19fe1296cf60c1fb92af
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2021-03-10 08:39:43 -0500

    Fix feedback from GTA -SK

--- rotc_app/lib/Views/signInPage.dart
+++ rotc_app/lib/Views/signInPage.dart
@@ -2,6 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
+import 'package:googleapis/doubleclickbidmanager/v1_1.dart';
+import 'package:rotc_app/services/auth.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../main.dart';
 
 /*
 Sawyer Kisha
@@ -12,23 +18,39 @@ Ui for the sign in page
 Co-Author:  Kyle Serruys
 Added Validation for the email and password properties
 */
+class SignInView extends StatefulWidget {
+  @override
+  _SignInViewState createState() => _SignInViewState();
+}
 
-class SignInView extends StatelessWidget {
-  CollectionReference cadres = FirebaseFirestore.instance.collection('cadres');
-  CollectionReference cadets = FirebaseFirestore.instance.collection('cadets');
+class _SignInViewState extends State<SignInView> {
+  CollectionReference users = FirebaseFirestore.instance.collection('users');
 
-  TextEditingController email = TextEditingController();
-  TextEditingController password = TextEditingController();
+  //Controllers for e-mail and password textfields.
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+
+  //Handling signup and signin
+  bool signUp = true;
+  bool isCadre;
+  final String firstName="";
+  final String lastName="";
+  final String nickname = "";
+  final String emailAddress ="";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            navigation.currentState.pushNamed('/welcomePage');
+          },
+        ),
         title: Text('Sign-In'),
-
-
       ),
-      body: Container(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(25.0),
         child: Form(
           // ignore: deprecated_member_use
@@ -39,15 +61,15 @@ class SignInView extends StatelessWidget {
               children: [
                 Padding(
                   padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
-                  child: Text('Username / Email: '),
+                  child: Text('Email: '),
                 ),
                 TextFormField(
                   controller: email,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    hintText: 'Username / Email',
+                    hintText: 'Insert Email',
                   ),
-                  onSaved: (String value) {},
+                  //onSaved: (String value) {},********************
                   validator: MultiValidator([
                     RequiredValidator(errorText: "Required"),
                     EmailValidator(errorText: "Not a valid email"),
@@ -65,10 +87,10 @@ class SignInView extends StatelessWidget {
                       controller: password,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: 'Password',
+                        hintText: 'Insert Password',
                       ),
                       obscureText: true,
-                      onSaved: (String value) {},
+                      // onSaved: (String value) {},******************
                       validator: MultiValidator([
                         MinLengthValidator(5,
                             errorText:
@@ -84,12 +106,33 @@ class SignInView extends StatelessWidget {
                               child: Text('Sign In'),
                               onPressed: () async {
                                 try {
+                                 /* context.read<Auth>().signIn(
+                                      email: email.text.trim(),
+                                      password: password.text.trim());*/
+
                                   UserCredential user = await FirebaseAuth
                                       .instance
                                       .signInWithEmailAndPassword(
-                                          email: email.text,
-                                          password: password.text);
-                                  Navigator.pushNamed(context, '/home');
+                                      email: email.text,
+                                      password: password.text);
+
+                                  var currentUser = await FirebaseAuth.instance.currentUser;
+                                  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+                                  var uid = currentUser.uid;
+
+                                  var data = await FirebaseFirestore.instance.collection('users').doc(uid).get().then((docSnapshot) {
+                                    return docSnapshot.data();
+                                  });
+
+                                  await prefs.setString('firstName', data['firstName'].toString());
+                                  await prefs.setString('lastName', data['lastName'].toString());
+                                  await prefs.setString('nickname', data['nickName'].toString());
+                                  await prefs.setString('email', currentUser.email);
+                                  await prefs.setString('isCadre', data['isCadre'].toString());
+
+
+                                  Navigator.pushNamed(context, '/homePage');
 
                                 } catch (e) {
                                   alertDialog(context);
